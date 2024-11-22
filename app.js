a01d26888b90b605c3ba6db33c647df6b545e1cc
motun02 <ifeojoomoniyi@gmail.com>
2024-11-22 15:57:46 +0100

    app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Simple form submission message
-document.getElementById("contactForm").addEventListener("submit",function(event)
+document.AbortController.getElementById("contactForm").addEventListener("submit",function(event)
 {
     event.preventDefault();
     alert("Thank you for your message! I'll get back to you soon.");
