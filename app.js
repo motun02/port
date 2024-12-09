f19cf98030bbed772aa833ab94072e7e711800e8
motun02 <ifeojoomoniyi@gmail.com>
2024-12-09 19:40:44 +0100

    edit

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ document.getElementById("contactForm").addEventListener("submit",function(event)
 {
     event.preventDefault();
     alert("Thank you for your message! I'll get back to you soon.");
-    document.getElementById("contactForm").
+    document.getElementById("contactForm")
     reset();
 });
